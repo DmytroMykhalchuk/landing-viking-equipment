91f5203cf671ec3c10a1ce7a87a3265c20c4ec8c
Dima Mykhalchuk <dimamuchlachuk@gmail.com>
2024-03-18 11:06:21 +0200

    Init.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import Stack from '@mui/material/Stack';
+import { AppLayout } from '../src/layout/AppLayout';
+import { FullScreenLoader } from '../src/modules/Common/FullScreenLoader';
+import { getIsFetching, getRedirectPath } from './redux/app/appSelector';
+import { removeRedirect } from './redux/app/appReducer';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
+import { ThemeWrapper } from '../src/modules/ui/ThemeWrapper';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+  const navigate = useNavigate();
+  const dispatch: any = useDispatch();
 
-export default App;
+  const redirectPath = useSelector(getRedirectPath);
+  const isFetching = useSelector(getIsFetching);
+
+  useEffect(() => {
+    if (!redirectPath) return;
+
+    dispatch(removeRedirect());
+    navigate(redirectPath);
+
+    // eslint-disable-next-line
+  }, [redirectPath]);
+
+
+  return (
+    <ThemeWrapper>
+      <Stack sx={{ bgcolor: 'background.default' }}>
+        {isFetching && (<FullScreenLoader />)}
+        <Routes>
+
+          <Route path='/*' element={
+            <Suspense fallback={<FullScreenLoader />}>
+              <AppLayout />
+            </Suspense>
+          } />
+
+        </Routes>
+      </Stack>
+    </ThemeWrapper>
+  );
+};
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from '../src/redux/store';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import './configs/i18n/i18nextConf';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+if (process.env.REACT_APP_STRICT_MODE === '1') {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  root.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (process.env.REACT_APP_ENV === 'local') {
+  reportWebVitals();
+} else if (process.env.REACT_APP_ENV === 'production') {
+}
